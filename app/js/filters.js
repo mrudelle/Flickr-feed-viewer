6700ae1dcd9324ca671113c0ab210ba0df484bb5
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-04-01 02:23:38 +0100

    Advanced date parsing
    Angular.js does not implement angular for locale and simplicity reason
    so this is a fix to print correct date format

--- app/js/filters.js
+++ app/js/filters.js
@@ -35,3 +35,17 @@ flickrfeedFilters.filter('handleUntitled', function() {
 			return input === "" ? "Untitled" : input;
 		};
 });
+
+flickrfeedFilters.filter('parseDate', function() {
+		return function(input) {
+			var Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+			var authorPatt = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}/;
+			var ordinal = function(nb){
+				var s = ["th","st","nd","rd"];
+				var v = nb % 100;
+				return nb+(s[(v-20)%10]||s[v]||s[0]);
+			}
+			var data = authorPatt.exec(input);
+			return ordinal(data[3]) + " " + Months[data[2]-1] + " " + data[1] + " at " + data[4] + ":" + data[5];
+		};
+});
